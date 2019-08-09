b85116aca5189aa7c6e93305c7c08613b8252571
sylwesterL <53795170+sylwesterL@users.noreply.github.com>
2019-08-09 20:36:48 +0100

    Add files via upload

---
+++ SP/Loops/Exercise2.java
@@ -0,0 +1,12 @@
+/*Napisz pętlę while, która wypisze na ekranie wszystkie liczby od 10 do 20 włącznie.*/
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+        int number = 10;
+        while (number <=20) {
+            System.out.print(number + "\t");
+            number++;
+        }
+    }
+}
